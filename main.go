0595c102e19c72f7d0b6d7301a4d67e7bbf17926
SouL-H <mhmtaydinw@gmail.com>
2022-05-25 22:24:21 +0300

    about handler added

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"net/http"
 )
-
+func aboutHandler(w http.ResponseWriter,r *http.Request){
+	w.Write([]byte("About page"))
+}
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello"))
 	})
+	http.HandleFunc("/about",aboutHandler)
 	http.ListenAndServe(":8081", nil)
 }
