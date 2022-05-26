db7eb9bcbe21ad824f35cd1d1fb13624e42db48c
SouL-H <mhmtaydinw@gmail.com>
2022-05-26 23:07:33 +0300

    basic queryString

---
+++ queryString.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/search", search)
+	http.ListenAndServe(":8082", nil)
+}
+
+func search(w http.ResponseWriter, r *http.Request) {
+	h1 := r.FormValue("h1")
+	source := r.FormValue("source")
+
+	data := "/search?h1=" + h1 + "&source=" + source
+	w.Write([]byte(data))
+
+}
