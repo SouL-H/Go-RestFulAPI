d5b1627e98ff00fd53d8b9a86e0b7b449b5a1bb6
SouL-H <mhmtaydinw@gmail.com>
2022-05-26 22:43:23 +0300

    path uri

--- main.go
+++ main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"net/http"
 )
-func aboutHandler(w http.ResponseWriter,r *http.Request){
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("About page"))
 }
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello user"))
+	x := r.URL.Path[1:]
+
+	w.Write([]byte(x))
+
+}
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello"))
-	})
-	http.HandleFunc("/about",aboutHandler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/index", indexHandler)
+	http.HandleFunc("/about", aboutHandler)
 	http.ListenAndServe(":8081", nil)
 }
